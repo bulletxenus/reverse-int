f963ffc1379f30753c44799634258913128fc0c8
Dmitry Khamitsevich <bullet.xenus@gmail.com>
2020-09-06 00:39:38 +0300

    fix: redefinition function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let array = " ";
+    for (i = str.length-1; i >= 0; i--) {
+        array = array + str[i];
+    }
+    return array;
 }
